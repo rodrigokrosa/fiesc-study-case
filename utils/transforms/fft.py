b8c66f213fcb6490c5162e8c6a0955618d61aac7
Rodrigo <rodrigokrosa@gmail.com>
2024-02-10 15:06:49 -0300

    refac: changing fft func to only return amplitudes

--- utils/transforms/fft.py
+++ utils/transforms/fft.py
@@ -4,19 +4,16 @@ from scipy.fft import fft, fftfreq
 
 def get_fft(
     data: np.array,
-    fs: int,
     remove_mean: bool = True,
-):
+) -> np.array:
     """Compute the one-dimensional discrete Fourier Transform.
 
     Parameters:
         data (array-like): Input data.
-        fs (int): Sampling frequency of the input data.
         remove_mean (bool, optional): Whether to remove the mean of the input data. Default is True.
 
     Returns:
-        freq (np.array): The frequencies of the spectrum.
-        mag_spectrum (np.array): The magnitude spectrum of the input data.
+        np.array: The magnitude spectrum of the input data.
     """
     # Number of samples
     n = int(len(data))
@@ -26,12 +23,25 @@ def get_fft(
         data = data - np.mean(data)
 
     # FFT
-    spectrum = fft(data, n=n) / n
-    freq = fftfreq(n, 1 / fs)
+    spectrum = fft(data, n=n, axis=0) / n
 
     # One-sided spectrum and frequencies
     mag_spectrum = np.abs(spectrum[: n // 2])
     mag_spectrum[1:] = mag_spectrum[1:] * 2
-    freq = freq[: n // 2]
 
-    return freq, mag_spectrum
+    return mag_spectrum
+
+
+def get_fftfreq(samples: int, fs: int) -> np.array:
+    """Calculate the frequencies of the FFT output.
+
+    Args:
+        samples (int): The number of samples in the input signal.
+        fs (int): The sampling frequency of the input signal.
+
+    Returns:
+        np.array: An array of frequencies corresponding to the FFT output.
+    """
+    freq = fftfreq(samples, 1 / fs)
+    freq = freq[: samples // 2]
+    return freq
